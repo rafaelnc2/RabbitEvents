da716f9b58f138e2951086ffc4fe6a34a96ad45a
Rafael Naville <rafael.nc2@gmail.com>
2024-07-21 13:32:48 -0300

    Remove comentários

--- RabbitEvents.Shared/Constants/QueueDefinitions.cs
+++ RabbitEvents.Shared/Constants/QueueDefinitions.cs
@@ -7,8 +7,6 @@ public static class QueueDefinitions
     public static Exchange AUTOR_EXCHANGE = new("autor_exchange", "topic");
 
     public static Queue AUTOR_ADD_QUEUE = new("autor_add_queue", "autor.#");
-    //public static string AUTOR_ADD_ROUTINGKEY = "autor_*"; //testar com curinga - tudo o que tiver "autor_" na rk deve chegar também nesta fila
 
     public static Queue AUTOR_IMAGE_QUEUE = new("autor_image_queue", "autor.image.#");
-    //public static string AUTOR_IMAGE_ROUTINGKEY = "*autor_image*"; //testar com curinga - tudo o que tiver "autor_image" na rk deve chegar também nesta fila
 }
